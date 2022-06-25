cc9a23d614b11f566d1ad58d5256f0025225f5c7
Derrick Yoo <derrickyoo@gmail.com>
2022-06-25 10:27:47 -0700

    add core-data lib

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
-import { MaterialModule } from '@fem-prod-ng/material';
+import { MaterialModule, materialRoutes } from '@fem-prod-ng/material';
+import { CoreDataModule, coreDataRoutes } from '@fem-prod-ng/core-data';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, HttpClientModule, MaterialModule],
+  imports: [BrowserModule, HttpClientModule, CoreDataModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent],
 })
