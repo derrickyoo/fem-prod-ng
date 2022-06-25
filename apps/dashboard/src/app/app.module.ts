db345a6954318854502b8cec22232124a68a0ec1
Derrick Yoo <derrickyoo@gmail.com>
2022-06-25 10:38:06 -0700

    add widgets service to core-data and routing module to dahsboard

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -6,10 +6,11 @@ import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MaterialModule, materialRoutes } from '@fem-prod-ng/material';
 import { CoreDataModule, coreDataRoutes } from '@fem-prod-ng/core-data';
+import { RoutingModule } from './routing.module';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, HttpClientModule, CoreDataModule, MaterialModule],
+  imports: [BrowserModule, HttpClientModule, CoreDataModule, MaterialModule, RoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
