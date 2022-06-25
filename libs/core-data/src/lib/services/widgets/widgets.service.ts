3f59cfb905da96572cafca753e2b93c3a53ba3c9
Derrick Yoo <derrickyoo@gmail.com>
2022-06-25 16:29:45 -0700

    Add serve:json command and update widgets service

--- libs/core-data/src/lib/services/widgets/widgets.service.ts
+++ libs/core-data/src/lib/services/widgets/widgets.service.ts
@@ -1,9 +1,43 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+// import { environment } from '@env/environment';
+import { IWidget } from '@fem/api-interfaces';
+
+const API_ENDPOINT = 'http://localhost:3000/'
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WidgetsService {
+  model = 'widgets';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  all() {
+    return this.http.get<IWidget[]>(this.getUrl());
+  }
+
+  find(id: string) {
+    return this.http.get<IWidget>(this.getUrlWithId(id));
+  }
+
+  create(widget: IWidget) {
+    return this.http.post(this.getUrl(), widget);
+  }
+
+  update(widget: IWidget) {
+    return this.http.put(this.getUrlWithId(widget.id), widget);
+  }
+
+  delete(widget: IWidget) {
+    return this.http.delete(this.getUrlWithId(widget.id));
+  }
+
+  private getUrl() {
+    return `${API_ENDPOINT}${this.model}`;
+  }
+
+  private getUrlWithId(id: string | null) {
+    return `${this.getUrl()}/${id}`;
+  }
 }
