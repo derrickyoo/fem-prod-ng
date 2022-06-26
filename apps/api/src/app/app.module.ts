fcca664e44ee44e31b2227ef4dd5ba73e06f0c84
Derrick Yoo <derrickyoo@gmail.com>
2022-06-26 09:16:35 -0700

    update nest widgets module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,13 @@
+import { WidgetsModule } from './../widgets/widgets.module';
 import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    WidgetsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
